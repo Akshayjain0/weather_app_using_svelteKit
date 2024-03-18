99f250416382656c0d3e921074a3f6d77eeb643b
Akshay Jain <akshayjain2823@gmail.com>
2024-03-18 22:04:35 +0530

    Initial structure

---
+++ weather_app/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Weather from "../components/+weather.svelte";
+
+</script>
+<div class="flex items-center justify-between bg-white">
+    <div class="left w-[30%] min-h-screen"><Weather/></div>
+    <div class="right w-[70%] bg-gray-100 min-h-screen">Right</div>
+</div>
