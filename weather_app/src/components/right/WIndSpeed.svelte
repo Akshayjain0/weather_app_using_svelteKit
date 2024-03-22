f93e9e87b5a8b25594da1b526c6f6113f6b38373
Akshay Jain <akshayjain2823@gmail.com>
2024-03-23 00:14:47 +0530

    Debug

--- weather_app/src/components/right/WIndSpeed.svelte
+++ weather_app/src/components/right/WIndSpeed.svelte
@@ -1,35 +1,46 @@
 <script lang="ts">
-	import { weatherData } from '../../store/weatherStore';
-	let weather: any;
-	weatherData?.subscribe((data) => {
-		weather = data;
-	});
+    import { weatherData } from '../../store/weatherStore';
+    import { onMount } from 'svelte';
+    import { onDestroy } from 'svelte';
 
-	function getWindDirection(degrees: number) {
-		if (degrees >= 337.5 || degrees < 22.5) {
-			return 'N';
-		} else if (degrees >= 22.5 && degrees < 67.5) {
-			return 'NE';
-		} else if (degrees >= 67.5 && degrees < 112.5) {
-			return 'E';
-		} else if (degrees >= 112.5 && degrees < 157.5) {
-			return 'SE';
-		} else if (degrees >= 157.5 && degrees < 202.5) {
-			return 'S';
-		} else if (degrees >= 202.5 && degrees < 247.5) {
-			return 'SW';
-		} else if (degrees >= 247.5 && degrees < 292.5) {
-			return 'W';
-		} else {
-			return 'NW';
-		}
-	}
+    let weather: any;
+    let windDegrees: number | undefined; 
 
-	// Example usage
-	// const windDegrees = weather?.wind?.deg; // Example wind degrees value
-	const windDirection = getWindDirection(
-		weather?.wind?.deg == undefined ? '90' : weather?.wind?.deg
-	);
+    const unsubscribe = weatherData?.subscribe((data) => {
+        weather = data;
+        windDegrees = weather?.wind?.deg; 
+    });
+
+ 
+    onDestroy(() => {
+        unsubscribe();
+    });
+
+ 
+    let windDirection: string = ''; // Initialize windDirection
+    function getWindDirection(degrees: number | undefined) {
+        if (degrees === undefined) return ''; 
+
+        if (degrees >= 337.5 || degrees < 22.5) {
+            return 'N';
+        } else if (degrees >= 22.5 && degrees < 67.5) {
+            return 'NE';
+        } else if (degrees >= 67.5 && degrees < 112.5) {
+            return 'E';
+        } else if (degrees >= 112.5 && degrees < 157.5) {
+            return 'SE';
+        } else if (degrees >= 157.5 && degrees < 202.5) {
+            return 'S';
+        } else if (degrees >= 202.5 && degrees < 247.5) {
+            return 'SW';
+        } else if (degrees >= 247.5 && degrees < 292.5) {
+            return 'W';
+        } else {
+            return 'NW';
+        }
+    }
+
+    $: windDirection = getWindDirection(windDegrees);
 </script>
 
 <div
